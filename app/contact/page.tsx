d93ad98c47784e3113ca3fb643770de976ca3c36
kimhabork <kimhab.ork@yahoo.com>
2026-03-01 22:04:37 +0700

    update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Mail, PhoneCall } from "lucide-react";
+import Link from "next/link"
 
 export default function ContactPage() {
   const [isLoading, setIsLoading] = useState(false);
